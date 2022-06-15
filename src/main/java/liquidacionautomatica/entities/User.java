98c959ebc1f50e8615e240873b6df042ef610bb4
Renzo Bayarri <renzobayarri@gmail.com>
2022-06-15 10:23:26 -0300

    Personalización directorio archivo credenciales

--- src/main/java/liquidacionautomatica/entities/User.java
+++ src/main/java/liquidacionautomatica/entities/User.java
@@ -16,39 +16,41 @@ import liquidacionautomatica.exceptions.CredentialsNotFound;
  */
 public class User {
 
-    private String pilagaUser;
-    private String pilagaPassword;
+  private String pilagaUser;
+  private String pilagaPassword;
 
-    public User() throws FileNotFoundException, IOException, CredentialsNotFound {
-        FileReader leerArchivoCredenciales = new FileReader("credenciales.txt");
-        BufferedReader contenidoCredenciales = new BufferedReader(leerArchivoCredenciales);
+  public User() throws FileNotFoundException, IOException, CredentialsNotFound {
+    String user = System.getenv("USERNAME");
+    System.out.println(user);
+    FileReader leerArchivoCredenciales = new FileReader("C:\\Users\\" + user + "\\credenciales.txt");
+    BufferedReader contenidoCredenciales = new BufferedReader(leerArchivoCredenciales);
 
-        this.pilagaUser = contenidoCredenciales.readLine();
-        this.pilagaPassword = contenidoCredenciales.readLine();
+    this.pilagaUser = contenidoCredenciales.readLine();
+    this.pilagaPassword = contenidoCredenciales.readLine();
 
-        validation();
+    validation();
+  }
+
+  public String getPilagaUser() {
+    return pilagaUser;
+  }
+
+  public void setPilagaUser(String pilagaUser) {
+    this.pilagaUser = pilagaUser;
+  }
+
+  public String getPilagaPassword() {
+    return pilagaPassword;
+  }
+
+  public void setPilagaPassword(String pilagaPassword) {
+    this.pilagaPassword = pilagaPassword;
+  }
+
+  public void validation() throws CredentialsNotFound {
+    if (this.pilagaPassword == null || this.pilagaUser == null) {
+      throw new CredentialsNotFound("No se encontraron todas las credenciales necesarias para acceder al sistema");
     }
-
-    public String getPilagaUser() {
-        return pilagaUser;
-    }
-
-    public void setPilagaUser(String pilagaUser) {
-        this.pilagaUser = pilagaUser;
-    }
-
-    public String getPilagaPassword() {
-        return pilagaPassword;
-    }
-
-    public void setPilagaPassword(String pilagaPassword) {
-        this.pilagaPassword = pilagaPassword;
-    }
-
-    public void validation() throws CredentialsNotFound {
-        if (this.pilagaPassword == null || this.pilagaUser == null) {
-            throw new CredentialsNotFound("No se encontraron todas las credenciales necesarias para acceder al sistema");
-        }
-    }
+  }
 
 }
